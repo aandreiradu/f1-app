bfedb1ca8d02dfa71b3a5d12dc01cc9e9a8c51ef
Andrei Radu <andreiradu@Andreis-MacBook-Pro`>
2022-08-15 01:19:42 +0300

    fixed httponly cookies, logout functionality

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,19 +1,17 @@
 const allowedOrigins = require('./allowedOrigins');
 
 
-// configure cors options, so the app accepts requests only from whitelisted adressess.
-
+// config corsoptions, only the whiteslisted adressess can access.
 const corsOptions = {
-    origin : (origin,callback) => {
-        console.log('origin',origin);
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null,true);
-        } else {
-            callback( new Error('Not allowed by cors'));
-        }
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by cors"));
+      }
     },
-    optionSuccessStatus: 200,
-    credentials: true
-}
+    optionsSuccessStatus: 200,
+    
+  };
 
 module.exports = corsOptions;
