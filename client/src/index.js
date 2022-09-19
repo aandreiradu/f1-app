a220f5a234b2b006659defae4abcde70b2a7f046
Andrei Radu <andreiradu@Andreis-MacBook-Pro`>
2022-09-19 22:32:04 +0300

    ready to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,11 @@ import 'remixicon/fonts/remixicon.css'
 // import { AuthContextProvider } from './store/auth-context';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('f1-app'));
 root.render(
